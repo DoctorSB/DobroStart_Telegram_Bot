abc1f8ccab1dd59ba2b9bee444517bc5f192d0f7
macbook <ikamil522@gmail.com>
2023-09-03 17:20:29 +0300

    all ready

--- tgbot/keyboards/reply.py
+++ tgbot/keyboards/reply.py
@@ -7,3 +7,12 @@ registration_button = [
 registration_keyboard = ReplyKeyboardMarkup(
     keyboard=registration_button, resize_keyboard=True,
     one_time_keyboard=True, input_field_placeholder='Зарегестрироваться')
+
+donation_button = [
+    [KeyboardButton(text="Пожертвовать")]
+]
+
+donation_keyboard = ReplyKeyboardMarkup(
+    keyboard=donation_button, resize_keyboard=True,
+    one_time_keyboard=True, input_field_placeholder='Пожертвовать')
+
